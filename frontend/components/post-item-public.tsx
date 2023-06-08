7892abe37e74e9ea33d30890325cfea15e18a22a
mikeKorakakis <30752591+mikeKorakakis@users.noreply.github.com>
2023-06-08 21:06:35 +0300

    changed posts public

--- frontend/components/post-item-public.tsx
+++ frontend/components/post-item-public.tsx
@@ -18,9 +18,11 @@ import Blocks from 'editorjs-blocks-react-renderer';
 
 
 
+
 interface PostItemProps {
 	post: Pick<Post, "id" | "title" | "published" | "createdAt" | "content">;
 }
+
 
 export const Article = (dataFromEditor:any) => <Blocks data={dataFromEditor} />;
 
@@ -31,9 +33,47 @@ export function PostItemPublic({ post }: PostItemProps) {
 		<div className="flex items-center justify-between p-4">
 			<div className="grid gap-1">           
 				{/* <Card> */}
-                
+                    <div className="prose pb-4"><h1>{post.title}</h1></div>
                     <Blocks 
                     data={post.content}
+                    config={{
+                        code: {
+                          className: "language-js"
+                        },
+                        delimiter: {
+                          className: "border border-2 w-16 mx-auto"
+                        },
+                        embed: {
+                          className: "border-0"
+                        },
+                        header: {
+                          className: "font-bold"
+                        },
+                        image: {
+                          className: "w-full max-w-screen-md",
+                          actionsClassNames: {
+                            stretched: "w-full h-80 object-cover",
+                            withBorder: "border border-2",
+                            withBackground: "p-2",
+                          }
+                        },
+                        list: {
+                          className: "list-disc list-inside"
+                        },
+                        paragraph: {
+                          className: "text-base text-opacity-75",
+                          actionsClassNames: {
+                            alignment: "text-{alignment}", // This is a substitution placeholder: left or center.
+                          }
+                        },
+                        quote: {
+                          className: "py-3 px-5 italic font-serif"
+                        },
+                        table: {
+                          className: "table-auto ",
+                        }
+
+                      }} 
                 />
 					{/* <CardHeader>
 						<CardTitle>
